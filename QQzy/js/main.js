4b618ed4d557b22b8770c22528123bcdb1c60917
xie <124695381+Carb114514@users.noreply.github.com>
2023-02-12 18:36:03 +0800

    Add files via upload

---
+++ QQzy/js/main.js
@@ -0,0 +1,28 @@
+function playMusic() {
+        var audioEle = document.getElementById("audio");
+      if (audioEle.paused){
+        audioEle.play();
+      }else {
+        audioEle.pause();
+      }
+    }
+    
+    $("#music").click(function(){
+      play();
+    });
+
+    var player = document.getElementById("audio");
+    play();
+    function play(){
+    swal("欢迎浏览玖爱三玖的QQ主页\nQQ 2250535092", {
+            buttons: {
+                cancel: "好的"
+            }
+        }).then(function(value) {
+            if (value == "allow") {
+                player.pause()
+            } else {
+                player.play();
+            }
+        });
+    }
